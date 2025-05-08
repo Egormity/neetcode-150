889e21f00e7c1aaa48c5ad0665be772ab84e7179
Egormity <kotlaregor9@gmail.com>
2025-05-08 13:38:07 +0700

    7 isValidSudoku optimal sokution

--- main.js
+++ main.js
@@ -1,52 +1,106 @@
 // ------------------------- # 7. 2025.05.07 - 1:08:36 | Leetcode 36 - Valid Sudoku - Medium ------------------------- //
 // My solution
-var isValidSudoku = function (board) {
-    let result = true;
-    for (const item of board) {
-        const dic = {};
-        item.forEach((el) => {
-            if (el === ".") return;
-            if (dic[el]) {
-                result = false;
-                dic[el] += 1;
-            } else dic[el] = 1;
-        });
-    }
+// var isValidSudoku = function (board) {
+//     let result = true;
+//     for (const item of board) {
+//         const dic = {};
+//         item.forEach((el) => {
+//             if (el === ".") return;
+//             if (dic[el]) {
+//                 result = false;
+//                 dic[el] += 1;
+//             } else dic[el] = 1;
+//         });
+//     }
 
-    //
-    if (!result) return result;
-    for (let i = 0; i < 3; i++) {
-        const items = board.slice(3 * i, 3 * (i + 1));
-        const rows = { 1: {}, 2: {}, 3: {} };
-        items.forEach((el) =>
-            el.forEach((char, i) => {
-                if (char === ".") return;
-                const row = rows[Math.ceil((i + 1) / 3) + ""];
-                if (row[char]) {
-                    result = false;
-                    row[char] += 1;
-                } else row[char] = 1;
-            })
-        );
-    }
-    if (!result) return result;
+//     //
+//     if (!result) return result;
+//     for (let i = 0; i < 3; i++) {
+//         const items = board.slice(3 * i, 3 * (i + 1));
+//         const rows = { 1: {}, 2: {}, 3: {} };
+//         items.forEach((el) =>
+//             el.forEach((char, i) => {
+//                 if (char === ".") return;
+//                 const row = rows[Math.ceil((i + 1) / 3) + ""];
+//                 if (row[char]) {
+//                     result = false;
+//                     row[char] += 1;
+//                 } else row[char] = 1;
+//             })
+//         );
+//     }
+//     if (!result) return result;
 
-    //
-    const items = board.slice(0);
-    const cols = {};
-    items.forEach((el) =>
-        el.forEach((char, i) => {
-            if (char === ".") return;
-            if (!cols[i + ""]) cols[i + ""] = {};
-            col = cols[i + ""];
-            if (col[char]) {
-                result = false;
-                col[char] += 1;
-            } else col[char] = 1;
-        })
-    );
-    console.log(cols);
-    return result;
-};
+//     //
+//     const items = board.slice(0);
+//     const cols = {};
+//     items.forEach((el) =>
+//         el.forEach((char, i) => {
+//             if (char === ".") return;
+//             if (!cols[i + ""]) cols[i + ""] = {};
+//             col = cols[i + ""];
+//             if (col[char]) {
+//                 result = false;
+//                 col[char] += 1;
+//             } else col[char] = 1;
+//         })
+//     );
+//     console.log(cols);
+//     return result;
+// };
 
 // Optimal solution
+// /**
+//  * @param {character[][]} board
+//  * @return {boolean}
+//  */
+// var isValidSudoku = function (board) {
+//     const columns = [];
+//     const boxes = [];
+
+//     //
+//     for (let rowI = 0; rowI < board.length; rowI++) {
+//         const row = board[rowI];
+//         rowDic = {};
+//         for (let charI = 0; charI < row.length; charI++) {
+//             const char = row[charI];
+//             if (char === ".") continue;
+//             if (char in rowDic) return false;
+//             rowDic[char] = true;
+//         }
+
+//         //
+//         for (let charI = 0; charI < row.length; charI++) {
+//             const char = row[charI];
+
+//             //
+//             if (columns[charI]) {
+//                 if (char !== "." && columns[charI].includes(char)) return false;
+//                 columns[charI].push(char);
+//             } else columns.push([char]);
+
+//             //
+//             const boxI = Math.floor(rowI / 3) * 3 + Math.floor(charI / 3);
+//             if (boxes[boxI]) {
+//                 if (char !== "." && boxes[boxI].includes(char)) return false;
+//                 boxes[boxI].push(char);
+//             } else boxes.push([char]);
+//         }
+//     }
+
+//     //
+//     return true;
+// };
+// console.log(
+//     isValidSudoku([
+//         [".", "8", "7", "6", "5", "4", "3", "2", "1"],
+//         ["2", ".", ".", ".", ".", ".", ".", ".", "."],
+//         ["3", ".", ".", ".", ".", ".", ".", ".", "."],
+//         ["4", ".", ".", ".", ".", ".", ".", ".", "."],
+//         ["5", ".", ".", ".", ".", ".", ".", ".", "."],
+//         ["6", ".", ".", ".", ".", ".", ".", ".", "."],
+//         ["7", ".", ".", ".", ".", ".", ".", ".", "."],
+//         ["8", ".", ".", ".", ".", ".", ".", ".", "."],
+//         ["9", ".", ".", ".", ".", ".", ".", ".", "."],
+//     ])
+// );
